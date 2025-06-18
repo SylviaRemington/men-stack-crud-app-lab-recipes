8f6af2185f09df51bab9a23797f24e6154809577
SylviaRemington <sylviaremington@gmail.com>
2025-06-18 04:26:38 -0700

    adding basic init code and structure for express app

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000; //can add this but not necessary
+
+app.get('/', (req, res) => {
+    res.send('Hello, supahstaaaars!');
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 })
