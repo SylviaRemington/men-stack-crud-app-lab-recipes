b86e18414d2afd594daa73ccefc11e7048d50c84
SylviaRemington <sylviaremington@gmail.com>
2025-06-18 14:39:52 -0700

    adding notes to server.js sections

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// ALL IMPORTS AT TOP (with dotenv at the most top so environmental variables working through all of this.)
 const dotenv = require('dotenv'); //initializing in JS
 dotenv.config(); //This gives us access to process.evn.MONGODB_URI
 const port = 3000; //can add this but not necessary
@@ -8,15 +9,19 @@ const mongoose = require('mongoose');
 //Importing the model into server.js
 const Recipe = require('./models/recipes.js')
 
+
+// Creating the app using Express, so can build out routes, handle requests & send responses
 const app = express();
+
 
 // MongoDB Connection - connection to the database //also connecting via mongoose gives more feedback vs try catch 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 });
 
+
+// ROUTES
 // app.get('/', async (req, res) => {
 //     res.send('Hello, supahstaaaars!'); //Checking if communicating with browser is working.
 // });
@@ -25,6 +30,8 @@ app.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
+
+// Starts the app and tells it to listen for requests on PORT (3000)
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
