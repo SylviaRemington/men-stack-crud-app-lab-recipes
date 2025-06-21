f258b2d87cfa4af0d834e761502d5341cfa19540
SylviaRemington <sylviaremington@gmail.com>
2025-06-20 20:21:33 -0700

    adding app.get and app.post functions

--- server.js
+++ server.js
@@ -54,18 +54,49 @@ app.get('/recipes/new', (req, res) => {
     res.render('recipes/new.ejs');
 });
 
+app.get('/recipes', async (req, res) => {
+    try {
+        const allRecipes = await Recipe.find();
+        res.render('recipes/index.ejs', { recipes: allRecipes });
+    } catch (err) {
+        res.status(500).send('Error loading recipes.');
+    }
+});
 
 // POST ROUTES
 // Need to set up post method route so that new.ejs can submit html forms
 // Setting up A CREATE ROUTE / we're setting up a post handler for a POST ROUTE
-app.post('/recipes', async (req, res) => {
-    console.log(req.body);
+// FIRST POST VERSION BELOW & THEN CHANGED IT TO NEXT VERSION BELOW:
+// app.post('/recipes', async (req, res) => {
+//     console.log(req.body);
     // Api functions work in async method and need to complete & do something before move on. 
     // Our api methods need to send something back in order to complete and move onto the next thing. 
     // Thus res.redirect below:
-    res.redirect("/recipes/new");
-});
+//     res.redirect("/recipes/new");
+// });
 
+app.post('/recipes', async (req, res) => {
+    try {
+        // Convert checkboxes from "on"/undefined to true/false
+        req.body.isFavoriteRecipe = req.body.isFavoriteRecipe === 'on';
+        req.body.isDelicious = req.body.isDelicious === 'on';
+        //This line checks if the checkbox value is exactly "on" — 
+        // if it is, it saves true; if it's missing (undefined) or anything else, 
+        // it saves false.
+
+        // Save the new recipe to the database
+        await Recipe.create(req.body);
+
+        // Redirect to the recipes list page after saving
+        res.redirect('/recipes');
+    } catch (err) {
+        // If something goes wrong (like a missing field), log the error
+        console.log('Error saving recipe:', err);
+
+        // Show a server error in the browser
+        res.status(500).send('Something went wrong while saving the recipe.');
+    }
+});
 
 
 // Starts the app and tells it to listen for requests on PORT (3000)
